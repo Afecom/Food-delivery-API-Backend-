561430541ebace6d7796280f86bd00bac15a3ef2
Nuru Kamil <knuru472@gmail.com>
2025-09-05 15:47:42 +0300

    login endpoint on point

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ const initDatabase = async () => {
 
 initDatabase()
 
-
 app.listen(3000, () => {
     console.log("Server is up and running on port 3000")
 })
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import argon from 'argon2'
 import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const signUp = (models) => {
     return async (req, res) => {
@@ -27,5 +29,36 @@ export const signUp = (models) => {
     }
 } 
 export const login = (models) => {
-    return async (req, res) => {}
+    return async (req, res) => {
+        const body = req.body
+        const userModel = models['User']
+        const { email, password } = body
+        if (!email || !password) return res.status(400).json({message: "Please provide email and password to login"})
+
+        try {
+            const user = await userModel.findOne({where: { email }})
+            if(!user) return res.status(401).json({message: "Invalid credentials"})
+            const is_correct_password = await argon.verify(user.password, password)
+            if(!is_correct_password){
+                res.status(401).json({message: "Invalid credentials"})
+            }
+            const access_secret = process.env.JWT_ACCESS_SECRET
+            const refresh_secret = process.env.JWT_REFRESH_SECRET
+            const access_token = jwt.sign({user_id: user.id, user_role: user.role}, access_secret, {expiresIn: "15m"})
+            const refresh_token = jwt.sign({user_id: user.id, user_role: user.role}, refresh_secret, {expiresIn: "7d"})
+            const user_obj = user.get({plain: true})
+            const {password: pwd, ...rst_user} = user_obj 
+            res.status(200).json({
+                message: "User logged in successfully",
+                access_token: access_token,
+                refresh_token: refresh_token,
+                user: rst_user
+            })
+        } catch (error) {
+            res.status(400).json({
+                message: "Couldn't login the user",
+                error: error.message || error
+            })
+        }
+    }
 } 
