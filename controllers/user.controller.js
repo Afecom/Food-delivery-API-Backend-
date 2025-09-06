b5f35ef7bc67236a1c56807cb5354d8ae89069e2
Nuru Kamil <knuru472@gmail.com>
2025-09-06 13:59:06 +0300

    minor fix to the user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,24 @@
 export const list_users = (models) => {
     return async (req, res) => {
-        res.json("hello auth test")
+        const user_model = models['User']
+        try {
+            const users = await user_model.findAll()
+            const returnUsers = []
+            if(!users) return res.status(404).json({message: "User not found"})
+            users.map((user) => {
+                const {password: pwd, ...rst_user} = user.toJSON()
+                returnUsers.push(rst_user)
+            })
+            res.status(200).json({
+                message: "Users fetched successfully",
+                users: returnUsers
+            })
+        } catch (error) {
+            res.status(400).json({
+                message: "Couldn't fetch users",
+                error: error.message || error
+            })
+        }
     }
 } 
 export const list_users_by_id = (models) => {
