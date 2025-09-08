227abb5e1e7f1604e4b6e1ab7e42982a4382bfaf
Nuru Kamil <knuru472@gmail.com>
2025-09-08 18:08:40 +0300

    menu deletion feature

--- controllers/menuItem.controller.js
+++ controllers/menuItem.controller.js
@@ -1,11 +1,69 @@
 export const getMenuItem = (models) => {
-    return async (req, res) => {}
+    return async (req, res) => {
+        const id = req.params.id
+        const menu_item_model = models['Menu_item']
+        const restaurant_model = models['Restaurant']
+        try {
+            const menu_item = await menu_item_model.findOne({
+                where: {id},
+                include: {model: restaurant_model, as: "restaurant"}
+            })
+            if(!menu_item) return res.status(404).json({message: "menu with the provided ID is not found"})
+            res.status(200).json({
+                message: "Menu fetched successfully",
+                menu_item
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "Couldn't fetch the menu",
+                error: error.message || error
+            })
+        }
+    }
 }
 
 export const updateMenuItem = (models) => {
-    return async (req, res) => {}
+    return async (req, res) => {
+        const id = req.params.id
+        const menu_item_model = models['Menu_item']
+        const allowed = ["name", "restaurant_id", "price"]
+        const updates = Object.fromEntries(
+            Object.entries(req.body).filter(([key]) => allowed.includes(key))
+        )
+        try {
+            const menu = await menu_item_model.findByPk(id)
+            if(!menu) return res.status(404).json({message: "menu not found"})
+            await menu.update(updates)
+            res.status(200).json({
+                Message: "Menu updated successfully",
+                menu
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "Couldn't update the menu",
+                error: error.message || error
+            })
+        }
+    }
 }
 
 export const deleteMenuItem = (models) => {
-    return async (req, res) => {}
+    return async (req, res) => {
+        const id = req.params.id
+        const menu_item_model = models['Menu_item']
+        try {
+            const menu = await menu_item_model.findByPk(id)
+            if(!menu) return res.status(404).json({message: "Menu not found"})
+            const deleted = await menu.destroy()
+            res.status(203).json({
+                message: "Menu deleted successfully",
+                deleted
+            })
+        } catch (error) {
+            res.status(500).json({
+                message: "Couldn't delete menu",
+                error: error.message || error
+            })
+        }
+    }
 }
--- routes/menuItem.route.js
+++ routes/menuItem.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { updateMenuItem, deleteMenuItem, getMenuItem } from '../controllers/menuItem.controller.js'
+import { isAuthorized } from "../middlewares/auth.middleware.js";
+const admin_auth = isAuthorized("Admin")
 
 const menuItemRouter = (models) => {
     const router = Router()
-    router.patch('/:id', updateMenuItem(models))
-    router.delete('/:id', deleteMenuItem(models))
+    router.patch('/:id', admin_auth, updateMenuItem(models))
+    router.delete('/:id', admin_auth, deleteMenuItem(models))
     router.get('/:id', getMenuItem(models))
     return router
 }
