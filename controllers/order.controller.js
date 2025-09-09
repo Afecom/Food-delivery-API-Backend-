c5b2484b5f3edf361ccf6b2f0236b2c12e95d460
Nuru Kamil <knuru472@gmail.com>
2025-09-09 13:01:19 +0300

    added order listing

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,3 +1,5 @@
+import verify_token from "../utils/tokenChecker.js"
+
 export const createOrder = (models) => {
     return async (req, res) => {
         const body = req.body
@@ -20,7 +22,39 @@ export const createOrder = (models) => {
 }
 
 export const listOrders = (models) => {
-    return async (req, res) => {}
+    return async (req, res) => {
+        const order_model = models['Order']
+        const access_token = req.headers['authorization'].split(" ")[1]
+        const token = verify_token(access_token, "access")
+        if(!token) return res.status(401).json({message: "Invalid token"})
+        const user_id = token.user_id
+        const user_role = token.user_role
+        try {
+            if(user_role === "Admin"){
+                const orders = await order_model.findAll()
+                if(!orders) return res.status(404).json({message: "order not found"})
+                return res.status(200).json({
+                    message: "Orders fetched successfully",
+                    orders
+                })
+            }
+            else{
+                const orders = await order_model.findAll({
+                    where: { user_id }
+                })
+                if(!orders) return res.status(404).json({message: "order not found"})
+                return res.status(200).json({
+                    message: "Orders fetched successfully",
+                    orders
+                })
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: "Couldn't list orders",
+                error: error.message || error
+            })
+        }
+    }
 }
 
 export const listOrderById = (models) => {
